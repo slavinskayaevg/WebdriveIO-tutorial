cd167c92729069f8bc07482ffc0f52468c7b77a9
Evgeniya Slavinskaya <slavinskayaevg@gmail.com>
2023-04-26 17:06:39 +0300

    update username selector

--- pageObjects/mainPage.js
+++ pageObjects/mainPage.js
@@ -10,7 +10,7 @@ class LoginPage extends Page {
      * define selectors using getter methods
      */
     get inputUsername () {
-        return $('#username');
+        return $('#Username');
     }
 
     get inputPassword () {
